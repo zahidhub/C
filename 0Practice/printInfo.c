704a2f9bb01cad25d06d0065e44dcc6f8c61caa5
Hasan Zahid <guszahidha@student.gu.se>
2025-02-01 04:14:48 +0100

    printInfo.c addded in 0Practice/C repo

---
+++ 0Practice/printInfo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Name   : Hasan Zahid\n"); //user nama
+
+    printf("DOB    : Jun 01, 0001\n"); //user date of birth
+
+    printf("Mobile : 99-9999999999"); //user mobile num
+
+    return 0; //successfull execution
+
+}
