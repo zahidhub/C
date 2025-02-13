2d56b820c7a1828fbb4be595dd839348b14d534f
Hasan Zahid <guszahidha@student.gu.se>
2025-02-13 09:59:57 +0100

    skeleton of wp4bonus/c

--- WP4bonus/main.c
+++ WP4bonus/main.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-void swapChar();
+void swapChar(char * char1, char * char2);
 
 int main(int argc, char * argv[]){
-    if (argv !=2) {
+    if (argc !=2) {
         printf("Error! No argument found");
         return 0;
-    } else if (argv[1] > 10) {
+    } else if (strlen(argv[1]) > 10) {
         printf("Error! No argument found");
         return 0;
     }
     
 
-    char word = argv[1];
+    char * word = argv[1];
     char result[] = {0};
 
     printf("Enter your word: ");
     scanf("%s", word);
 
     for (int i= 0; i < strlen(word); i++){
-
-
+        
     }
     
 }
@@ -31,6 +30,7 @@ void swapChar(char * char1, char * char2){
     char temp;
     temp = * char1;
     * char1 = * char2;
-    * char2 = char1;
+    * char2 = * char1;
 
 }
+
